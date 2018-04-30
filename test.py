287536ae80eb0e961ce5284d223a977b48bd05ee
renato.santos <renato.santos@rivendel.com.br>
2018-04-30 17:59:37 -0400

    Update melo

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Melo.")
+        self.assertRegex(result.data.decode(), "JOAO MEU OVO.")
 
 
 if __name__ == "__main__":
